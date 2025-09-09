df4d758a73fc159cd165cb7840887848a6905e2f
pioner14 <pioner14@gmail.com>
2025-09-09 12:56:15 +0200

    Initial commit: Project setup and basic structure

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function(eleventyConfig) {
+  // Pass through (copy) static assets to the output directory (_dist)
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/inwidget");
+  eleventyConfig.addPassthroughCopy("src/.htaccess");
+
+  return {
+    // Define source and output directories
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_dist"
+    },
+    // All HTML files will be processed by the Liquid template engine
+    htmlTemplateEngine: "liquid"
+  };
+};
